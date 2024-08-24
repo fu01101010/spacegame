be171e863673233c7e6808d59148230c357a3b2e
fu01101010 <j0s3phv3rm33r@gmail.com>
2024-08-24 22:35:14 +0300

    added stb_image_h

--- source/main.cpp
+++ source/main.cpp
@@ -12,6 +12,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <stb/stb_image.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
